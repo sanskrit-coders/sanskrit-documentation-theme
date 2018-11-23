47d5362dee85429b62e0f26544c1cabcca8b8715
vvasuki <vishvas.vasuki@gmail.com>
2018-11-23 14:24:56 -0800

    Content update.

--- assets/js/on_doc_ready/audio_embed.js
+++ assets/js/on_doc_ready/audio_embed.js
@@ -1,30 +1,31 @@
-function mp3_embed() {
-    var p = document.getElementsByTagName('p');
-    for(var i = 0; i < p.length; i++) {
-        if(p[i].innerHTML.indexOf('.mp3') !== -1) {
-            var str = p[i].innerHTML.split('?');
-            if(str.length == 1) str[1] = '';
-            var str1 = str[1];
-            str1 = str1.replace('&','').replace('&','');
-            str1 = str1.replace('autoplay=1','').replace('autoplay=0','');
-            str1 = str1.replace('loop=1','').replace('loop=0','');
-            str1 = str1.replace('controls=0','').replace('controls=1','');
+function mp3Embed(audioEmbedTag) {
+    var src = audioEmbedTag.getAttribute( "src" );
+    if(src.indexOf('.mp3') !== -1) {
+        var srcParts = src.split('?');
+        if(srcParts.length == 1) srcParts[1] = '';
+        var unrecognizedParameterString = srcParts[1];
+        unrecognizedParameterString = unrecognizedParameterString.replace('&','').replace('&','');
+        unrecognizedParameterString = unrecognizedParameterString.replace('autoplay=1','').replace('autoplay=0','');
+        unrecognizedParameterString = unrecognizedParameterString.replace('loop=1','').replace('loop=0','');
+        unrecognizedParameterString = unrecognizedParameterString.replace('controls=0','').replace('controls=1','');
 
-            if (str[0].lastIndexOf('.mp3', str[0].length - 4) === str[0].length - 4 && str1.length == 0) {
-                if(str[1].indexOf('autoplay=1') !== -1) var autoplay=1; else var autoplay=0;
-                if(str[1].indexOf('loop=1') !== -1) var loop=1; else var loop=0;
-                if(str[1].indexOf('controls=0') !== -1) var controls=0; else var controls=1;
-                var newInnerHTML = '<audio';
-                if(autoplay==1) newInnerHTML += ' autoplay';
-                if(loop==1) newInnerHTML += ' loop';
-                if(controls==1) newInnerHTML += ' controls';
-                newInnerHTML += '><source src="'+str[0]+'" type="audio/mpeg">Your browser does not support the audio element, but here is <a href="' + str[0] + '">a link.</a></audio>';
-                p[i].innerHTML = newInnerHTML;
-            }
+        if (srcParts[0].lastIndexOf('.mp3', srcParts[0].length - 4) === srcParts[0].length - 4 && unrecognizedParameterString.length == 0) {
+            if(srcParts[1].indexOf('autoplay=1') !== -1) var autoplay=1; else var autoplay=0;
+            if(srcParts[1].indexOf('loop=1') !== -1) var loop=1; else var loop=0;
+            if(srcParts[1].indexOf('controls=0') !== -1) var controls=0; else var controls=1;
+            var newInnerHTML = '<audio';
+            if(autoplay==1) newInnerHTML += ' autoplay';
+            if(loop==1) newInnerHTML += ' loop';
+            if(controls==1) newInnerHTML += ' controls';
+            newInnerHTML += '><source src="'+srcParts[0]+'" type="audio/mpeg">Your browser does not support the audio element, but here is <a href="' + srcParts[0] + '">a link.</a></audio>';
+            audioEmbedTag.innerHTML = newInnerHTML;
         }
     }
 }
 
 $(document).ready(function() {
-    mp3_embed();
+    var audioEmbedTags = document.querySelectorAll('.audioEmbed');
+    audioEmbedTags.forEach(function (audioEmbedTag) {
+        mp3Embed(audioEmbedTag);
+    });
 });
--- subfolder1/media_embeding_test.md
+++ subfolder1/media_embeding_test.md
@@ -3,8 +3,8 @@ title: Media embedding test
 ---
 
 ## Video
-https://www.youtube.com/watch?v=uDd3iupKUyI
+<div class="videoEmbed" src="https://www.youtube.com/watch?v=uDd3iupKUyI"></div>
+
 
 ## Mp3
-https://archive.org/download/KrishnaYajurvedaCompleteAudioKannadaText/YajurvedaPart11.mp3
-
+<div class="audioEmbed" src="https://archive.org/download/KrishnaYajurvedaCompleteAudioKannadaText/YajurvedaPart11.mp3"></div>
