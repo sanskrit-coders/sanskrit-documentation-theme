6e6579ff9759600a80df4afcbfdf49352bf00e06
vvasuki <vishvas.vasuki@gmail.com>
2018-12-02 12:20:02 -0800

    Content update.

--- assets/js/on_doc_ready/sidebar.js
+++ assets/js/on_doc_ready/sidebar.js
@@ -1,6 +1,71 @@
 $('#displayed_sidebar').height($(".nav").height());
 
+function getSidebarItemHtml(sidebarItem) {
+    var item_url_stripped = sidebarItem.url || "#";
+    item_url_stripped = item_url_stripped.replace("index.html", "").replace(".md", ".html");
+
+    var urlTarget = "";
+    if (item_url_stripped.startsWith("http://") || item_url_stripped.startsWith("https://") || item_url_stripped.startsWith("ftp://")) {
+        urlTarget = "_newTab";
+    }
+    var list_item_css_class = "inactive";
+    if (pageSettings.url == item_url_stripped) {
+        list_item_css_class = "active";
+    }
+    // console.debug(sidebarItem);
+    if(sidebarItem.hasOwnProperty("contents")) {
+        var contentHtml = "";
+        for(let subitem of sidebarItem.contents) {
+            contentHtml = `${contentHtml}\n ${getSidebarItemHtml(subitem)}`;
+        }
+        var itemHtml = `<li><a href="${item_url_stripped}"> ${sidebarItem.title}</a>\n<ul>${contentHtml}\n</ul>\n</li>\n`;
+    } else if (sidebarItem.url.startsWith("dir://")) {
+        var dirUrl = sidebarItem.url.replace("dir://", "/");
+        if (dirUrl.endsWith("/")) {
+            dirUrl = dirUrl.slice(0,-1);
+        }
+        if (dirUrl in pageDirectoryToUrl) {
+            var itemHtml = "";
+            // console.debug(dirUrl);
+            for (let contentUrl of pageDirectoryToUrl[dirUrl]) {
+                var subitem = {"url": contentUrl};
+                itemHtml = `${itemHtml}\n ${getSidebarItemHtml(subitem)}`;
+            }
+        } else {
+            console.error(`No such directory ${dirUrl}`)
+        }
+    }
+    else {
+        var title = sidebarItem.title || pageUrlToTitle[sidebarItem.url];
+        var itemHtml = `<li class="${list_item_css_class}"><a href="${siteBaseurl + item_url_stripped }" target="">${title}</a></li>`;
+    }
+    return itemHtml;
+}
+
+function insertSidebarItems() {
+    var sidebar = siteData.sidebars[pageSettings.sidebar];
+    $("#displayed_sidebar .sidebarTitle").html(sidebar.title);
+    // console.debug(sidebar);
+    for (let sidebarItem of sidebar.contents) {
+        $("#displayed_sidebar").append(getSidebarItemHtml(sidebarItem));
+    }
+    // this highlights the active parent class in the navgoco sidebar. this is critical so that the parent expands when you're viewing a page. 
+    $("li.active").parents('li').toggleClass("active");
+}
+
+function insertTopnavDropdownItems() {
+    var topnavDropdown = siteData.sidebars[pageSettings.topnav];
+    console.debug(topnavDropdown);
+    for (let item of topnavDropdown.contents) {
+        $("#topnav_dropdown").append(getSidebarItemHtml(item));
+    }
+    // this highlights the active parent class in the navgoco sidebar. this is critical so that the parent expands when you're viewing a page. 
+    $("li.active").parents('li').toggleClass("active");
+}
+
 $(document).ready(function() {
+    insertSidebarItems();
+    insertTopnavDropdownItems();
     // Initialize navgoco sidebar with default options
     $("#displayed_sidebar").navgoco({
         caretHtml: '',
